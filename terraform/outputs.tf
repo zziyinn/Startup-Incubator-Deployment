f219be3dda20f77cae0f8bb02af952be25125186
zziyinn <ziyinzx@gmail.com>
2025-04-16 20:16:58 -0400

    feat: update deployment configuration

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,4 @@
 output "ec2_public_ip" {
-  description = "The public IP address of the EC2 instance"
-  value       = aws_instance.app.public_ip
+  description = "The public IP of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
 }
-
-output "ec2_public_dns" {
-  description = "The public DNS name of the EC2 instance"
-  value       = aws_instance.app.public_dns
-} 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-2"
 }
 
 variable "instance_type" {
